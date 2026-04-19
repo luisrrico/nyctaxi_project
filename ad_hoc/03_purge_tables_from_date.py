af46e1758bb379c8dffcf357776138ca1df22349
luisrrico <luisrguezrico97@gmail.com>
2026-04-19 15:25:28 +0000

    Adding purge tables from date

--- ad_hoc/03_purge_tables_from_date.py
+++ ad_hoc/03_purge_tables_from_date.py
@@ -16,7 +16,7 @@ from delta.tables import DeltaTable
 
 dt = DeltaTable.forName(spark, "nyctaxi.`01_bronze`.yellow_trips_raw")
 
-dt.delete(f"tpep_pickup_datetime >= {date_from}")
+dt.delete(f"tpep_pickup_datetime >= '{date_from}'")
 
 # COMMAND ----------
 
@@ -24,7 +24,7 @@ from delta.tables import DeltaTable
 
 dt = DeltaTable.forName(spark, "nyctaxi.`02_silver`.yellow_trips_cleansed")
 
-dt.delete(f"tpep_pickup_datetime >= {date_from}")
+dt.delete(f"tpep_pickup_datetime >= '{date_from}'")
 
 # COMMAND ----------
 
@@ -32,7 +32,7 @@ from delta.tables import DeltaTable
 
 dt = DeltaTable.forName(spark, "nyctaxi.`02_silver`.yellow_trips_enriched")
 
-dt.delete(f"tpep_pickup_datetime >= {date_from}")
+dt.delete(f"tpep_pickup_datetime >= '{date_from}'")
 
 # COMMAND ----------
 
@@ -40,4 +40,4 @@ from delta.tables import DeltaTable
 
 dt = DeltaTable.forName(spark, "nyctaxi.`03_gold`.daily_trip_summary")
 
-dt.delete(f"pickup_date >= {date_from}")
+dt.delete(f"pickup_date >= '{date_from}'")
